a75e5169323dd72a14f4fef82e1c5b8a24429601
Melf <cqc@melf.de>
2024-04-17 10:42:44 +0100

    update pytket version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license="Apache 2",
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
-    install_requires=["pytket ~= 1.26", "stim ~= 1.12"],
+    install_requires=["pytket ~= 1.27", "stim ~= 1.12"],
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.10",
